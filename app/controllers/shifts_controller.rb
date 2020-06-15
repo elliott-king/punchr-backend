bb8eacb6acd1d930aa68ff5283863721c7140045
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-06-15 11:24:20 -0400

    Sort shifts when passed to the frontend

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,7 +1,7 @@
 class ShiftsController < ApplicationController
   def index
     # FIXME: should take optional start, end date params
-    render :json => Shift.all, :include => {:user => {:only => [:first_name, :last_name]}}
+    render :json => Shift.sorted, :include => {:user => {:only => [:first_name, :last_name]}}
   end
 
   def current
--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,6 +1,11 @@
 class Shift < ApplicationRecord
   belongs_to :user
 
+  def self.sorted
+    # Apparently Postgres puts NULLs at end of list, so I don't need to implement my own sort fn.
+    Shift.order(:end, :start)
+  end
+
   def self.current
     shifts = []
     User.all.each do |user|
@@ -9,12 +14,12 @@ class Shift < ApplicationRecord
         shifts << s 
       end
     end
-    return shifts
+    return shifts.sort_by{|shift| shift.start}
   end
 
   def self.over_dates(start, ending)
     shifts = []
-    Shift.all.each do |shift|
+    Shift.sorted.each do |shift|
       if shift.end
         start_in_range = (shift.start < ending && shift.start > start)
         end_in_range = (shift.end < ending && shift.end > start)
